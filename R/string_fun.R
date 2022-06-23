d5548d22fd9e059be15cef873f31872d57e75342
Hansjörg Neth <hansneth@gmail.com>
2022-06-23 12:52:34 +0200

    minor

--- R/string_fun.R
+++ R/string_fun.R
@@ -4,16 +4,24 @@
 # Functions for manipulating/transforming character strings: ------ 
 
 
-# Turn a vector into a string: ------
+# vec2str: Turn a vector of symbols into a character string: ------
 
-vec2str <- function(s) {
+# (See collapse_chars() AND chars_to_text() in ds4psy package.)
+
+vec2str <- function(v) {
   
-  paste(s, collapse = "")
+  paste(v, collapse = "")
+  
+  # Note: Simply using paste(v, collapse = "") loses all spaces.
+  #       ds4psy::chars_to_text() preserves spaces. 
   
 } # vec2str(). 
 
 
-# Turn a string into a vector (of 1-symbol elements): ------ 
+
+# str2vec: Turn a character string into a vector (of 1-symbol character elements): ------ 
+
+# (See text_to_chars() in ds4psy package.)
 
 str2vec <- function(s){
   
