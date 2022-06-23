03b22b13b0d32534a2fa89635bd463dfaab372eb
Hansjörg Neth <hansneth@gmail.com>
2022-06-23 08:00:06 +0200

    base2dec: document and export

--- R/num_fun.R
+++ R/num_fun.R
@@ -1,15 +1,47 @@
 ## num_fun.R  | i2ds
-## hn | uni.kn | 2022 06 22
+## hn | uni.kn | 2022 06 23
 
 # Functions for transforming/manipulating numbers: ------ 
 
-# base2dec: Convert a base N numeric string into a decimal number: ---- 
+
+# base2dec: Convert a base N numeric string into a decimal number: ------ 
+
+#' Convert a base N numeric string (to a decimal numeric string). 
+#' 
+#' \code{base2dec} converts a sequence of numeric symbols 
+#' from base N notation to decimal (i.e., base 10) notation. 
+#' 
+#' @return A decimal number. 
+#' 
+#' @param seq A (required) sequence or string of numeric symbols 
+#' (i.e., digits 0-9, as defined in specified base).
+#' 
+#' @param base The base of the symbols in \code{seq}. 
+#' Default: \code{base = 2} (binary).   
+#'        
+#' @examples 
+#' # (a) string inputs:
+#' base2dec("0")
+#' base2dec("1")
+#' base2dec("1010")  # seq as string
+#' # (b) numeric vectors as inputs:
+#' base2dec(c(1, 0, 1, 0))
+#' base2dec(c(1, 0, 1, 0), base = 3)
+#' 
+#' @family numeric functions
+#' 
+#' @seealso
+#' \code{\link{as.roman}} converts integers into Roman numerals. 
+#' 
+#' @export 
 
 base2dec <- function(seq, base = 2){
   
+  # Initialize: 
   dec_nr  <- 0   
   len_seq <- length(seq)
   
+  # Prepare:
   if ((len_seq == 1) && (is.character(seq))) { 
     
     # convert string into vector (of 1-digit numeric elements):
@@ -19,9 +51,10 @@ base2dec <- function(seq, base = 2){
     
   } # if. 
   
+  # Main:   
   rev_seq <- rev(seq)
   
-  for (i in 1:len_seq){
+  for (i in 1:len_seq){ # compute polynomial: 
     
     cur_i  <- rev_seq[i]
     dec_nr <- dec_nr + (cur_i * base^(i-1))
@@ -32,7 +65,7 @@ base2dec <- function(seq, base = 2){
   
 } # base2dec(). 
 
-# # Check:
+# # # Check:
 # # (a) string inputs:
 # base2dec("0")
 # base2dec("1")
