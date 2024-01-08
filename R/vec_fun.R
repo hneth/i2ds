72c42ce9464b04262fe95ee2322245fdafb1a11d
hneth <hansneth@gmail.com>
2024-01-08 17:54:04 +0100

    add sort_by_names() function

--- R/vec_fun.R
+++ R/vec_fun.R
@@ -1,9 +1,26 @@
 ## vec_fun.R  | i2ds
-## hn | uni.kn | 2023 12 29
+## hn | uni.kn | 2024 01 08
 
 # Functions for transforming/manipulating vectors: ------ 
 #
 # Note that objects of type "table" are specific cases of arrays (contingency tables with freq. counts).
+
+
+# sort_by_names: Sort a vector by its element names ------
+
+sort_by_names <- function(v, ...){
+  
+  v[order(names(v), ...)]
+  
+} # sort_by_names().
+
+# # Check:
+# v <- 1:10
+# names(v) <- sample(letters[1:10], 10)
+# 
+# sort_by_names(v)
+# sort_by_names(v, decreasing = TRUE)
+
 
 
 # sort_in_order: Sort a vector x into the order given by the elements of order: ------ 
@@ -26,6 +43,8 @@ sort_in_order <- function(x, order){
 # sort_in_order(c(2, 1, 4, 3, 4, 3, 2, 1), order = c(3, 5, 8))
 # sort_in_order(c("C", "A", "X", "B", "Y", "A", "Z"), order = LETTERS)
 # sort_in_order(c("C", "A", "X", "B", "Y", "A", "Z"), order = rev(LETTERS))
+
+
 
 
 ## Done: ------ 
